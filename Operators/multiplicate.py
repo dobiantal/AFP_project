91aab3967f198c611034ef0d03a5770a7c6db280
FireWolfy89 <dmate899@gmail.com>
2023-11-28 22:58:00 +0100

    Szorzás művelet kiegészítve egy kisebb ellenőrzéssel

--- Operators/multiplicate.py
+++ Operators/multiplicate.py
@@ -1,4 +1,4 @@
 def multiplicate(OPERANDUS_A, OPERANDUS_B):
-    if OPERANDUS_B == 0:
-        raise ValueError("Az OPERANDUS_B értéke nem lehet 0.")
+    if OPERANDUS_B or OPERANDUS_A == 0:
+        raise ValueError("Az OPERANDUS_B vagy OPERANDUS_A értéke nem lehet 0.")
     return OPERANDUS_A * OPERANDUS_B
