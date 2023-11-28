0c6276a8290202eb352503c00dd9c4133fc00593
FireWolfy89 <dmate899@gmail.com>
2023-11-28 22:27:47 +0100

    Szorzás művelet

--- Operators/multiplicate.py
+++ Operators/multiplicate.py
@@ -1,2 +1,4 @@
-def multiplicate(OPERANDUS_A,OPERANDUS_B):
+def multiplicate(OPERANDUS_A, OPERANDUS_B):
+    if OPERANDUS_B == 0:
+        raise ValueError("Az OPERANDUS_B értéke nem lehet 0.")
     return OPERANDUS_A * OPERANDUS_B
