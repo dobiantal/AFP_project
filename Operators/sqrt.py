7a92665c49dd5c2246c8551f0f109f55c8124bd7
totyka97 <tamas970906@gmail.com>
2023-11-28 20:46:22 +0100

    A négyzetgyököt számító metódus implementálva lett.

--- Operators/sqrt.py
+++ Operators/sqrt.py
@@ -1,2 +1,11 @@
-def sqrt():
-    return None
+import math
+
+def negyzetgyok(OPERANDUS_A):
+    #Ha negatív számot kap 0 val tér vissza
+    if(OPERANDUS_A<0):
+        return 0;
+
+    return math.sqrt(OPERANDUS_A)
+
+
+
