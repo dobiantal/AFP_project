998fdbabd0e5aa163e0b8627b4501f75eb768147
Sz. Renáta <63110174+szr087@users.noreply.github.com>
2023-11-29 22:57:44 +0100

    Módosítás subtraction.py
    módosított kód

--- Operators/subtraction.py
+++ Operators/subtraction.py
@@ -1,28 +1,36 @@
-class Subtraction:
+from decimal import Decimal,getcontext,InvalidOperation
+
+class Subtraction():
+
+    def __init__(self,expression):
+        self.expression = expression
+        self.result = None
+
     global1 = ""
     global2 = ""
- 
+
     @staticmethod
     def subtract(expression):
         numbers = [Decimal(num) for num in expression.split('-') if num]
         result = numbers[0] if numbers else Decimal(0)
 
         for num in numbers[1:]:
-            result = operator.sub(result, num)
+            result -= num
 
         Subtraction.global1 = expression
         Subtraction.global2 = format(result, '.8f')
 
         return format(result, '.8f')
 
-
     def calculate(self):
         getcontext().prec = 8  # Beállítottam a tizedesjegy pontosságot
 
-        try:
-            if '-' in self.expression:
+        if '-' in self.expression:
+            try:
                 self.result = Subtraction.subtract(self.expression)
-            else:
-                raise ValueError("Érvénytelen kivonási kifejezés!")
-        except (ValueError, SyntaxError, InvalidOperation):
-            raise ValueError("Érvénytelen kifejezés!")
+            except (ValueError, SyntaxError, InvalidOperation) as e:
+                return str(e)  # Visszatérés hibaüzenettel
+        else:
+            return "Hiba!"
+
+
